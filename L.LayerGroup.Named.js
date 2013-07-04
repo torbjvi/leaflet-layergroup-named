b52ce72765d299134f343c6e2499daf91c6fecd0
Torbjørn Auglænd Vilhelmsen <torbjvi@stud.ntnu.no>
2013-07-04 20:18:09 +0200

    Create L.LayerGroup.Named.js

---
+++ L.LayerGroup.Named.js
@@ -0,0 +1,23 @@
+L.LayerGroup.Named = L.LayerGroup.extend({
+    addLayer: function (name, layer) {
+        var id = name;
+        this._layers[name] = layer;
+        if (this._map) {
+            this._map.addLayer(layer);
+        }
+        return this;
+    },
+    removeLayer: function (name) {
+        if (this._map && this._layers[name]) {
+            this._map.removeLayer(this._layers[name]);
+        }
+        delete this._layers[name];
+        return this;
+    },
+    getLayer: function (name) {
+        return this._layers[name];
+    }
+});
+L.layerGroup.named = function () {
+    return new L.LayerGroup();
+};
